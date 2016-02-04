d0420671595eb0a4b436ba2843462a8a667815cc
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-04 10:57:42 +0100

    Mise en œuvre du design responsive - CSS uniquement
    --
    tag: step-30-20

--- DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactRenderConfigView.php
+++ DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactRenderConfigView.php
@@ -4,5 +4,18 @@ namespace DduiTuto;
 
 class ContactRenderConfigView extends \Dcp\Ui\DefaultView
 {
+    public function getCssReferences(\Doc $document = null)
+    {
+        $version = \ApplicationParameterManager::getParameterValue(
+            "CORE", "WVERSION"
+        );
 
+        $cssReferences = parent::getCssReferences($document);
+
+        $cssReferences['DDUI_TUTO_CONTACT_view']
+            = "DDUI_TUTO/Families/DDUI_TUTO_CONTACT/Layout/view.css?ws="
+            . $version;
+
+        return $cssReferences;
+    }
 }
