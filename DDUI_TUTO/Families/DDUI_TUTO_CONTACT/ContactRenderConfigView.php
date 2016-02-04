d68e7d87f21a38f174f261ee3910d81b1a44774f
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-04 18:05:09 +0100

    Mise en œuvre du design responsive - Utilisation des templates
    --
    tag: step-30-30
    tag: step-40-00

--- DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactRenderConfigView.php
+++ DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactRenderConfigView.php
@@ -4,6 +4,34 @@ namespace DduiTuto;
 
 class ContactRenderConfigView extends \Dcp\Ui\DefaultView
 {
+    public function getTemplates(\Doc $document = null)
+    {
+        $templates = parent::getTemplates($document);
+
+        $templates["sections"]["content"]["file"]
+            = "DDUI_TUTO/Families/DDUI_TUTO_CONTACT/Layout/contactContent.mustache";
+        return $templates;
+    }
+
+    public function getJsReferences(\Doc $document = null)
+    {
+        $version = \ApplicationParameterManager::getParameterValue(
+            "CORE", "WVERSION"
+        );
+
+        $jsReferences = parent::getJsReferences();
+
+        $jsReferences["bootstrap_collapse"]
+            = "lib/bootstrap/3/js/collapse.js?ws="
+            . $version;
+
+        $jsReferences["jsqr"]
+            = "lib/jsqr/jsqr-1.0.2-min.js?ws="
+            . $version;
+
+        return $jsReferences;
+    }
+
     public function getCssReferences(\Doc $document = null)
     {
         $version = \ApplicationParameterManager::getParameterValue(
