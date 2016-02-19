36037efba137d5dbe950d2fda6f055a08fee2ee7
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-19 15:50:45 +0100

    Mise en place des vues - Partie serveur : Personnalisation du menu
    --
    tag: step-40-40

--- DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactWizardRenderConfigEdit.php
+++ DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactWizardRenderConfigEdit.php
@@ -2,6 +2,9 @@
 
 namespace DduiTuto;
 
+use Dcp\Ui\ElementMenu;
+use Dcp\Ui\ItemMenu;
+
 class ContactWizardRenderConfigEdit extends \Dcp\Ui\DefaultEdit
 {
     use TContactWizardRenderConfigEdit;
@@ -40,5 +43,66 @@ class ContactWizardRenderConfigEdit extends \Dcp\Ui\DefaultEdit
             . $version;
 
         return $cssReferences;
+    }
+
+    public function getMenu(\Doc $document)
+    {
+        $this->initWizardInfos($document);
+
+        $menu = parent::getMenu($document);
+
+        //Hide some menus
+        foreach(['save', 'create', 'createAndClose', 'close', 'workflow'] as $elementId) {
+            $element = $menu->getElement($elementId);
+            if (!is_null($element)) {
+                $element->setVisibility(ElementMenu::VisibilityHidden);
+            }
+        }
+
+        //add custom menus
+        $item = new ItemMenu(
+            "wizard_end", ___("End creation", "ddui_tuto:wizard"),
+            "#action/wizard.end"
+        );
+        $item->setBeforeContent('<div class="fa fa-check" />');
+        $item->setHtmlAttribute("class", "menu--right");
+        if (\DduiTuto\DDUI_TUTO_CONTACT__WFL::e_up_to_date === $document->getState()
+            || !empty($this->wizardInfos['nextStep'])) {
+            $item->setVisibility($item::VisibilityHidden);
+        }
+        $item->useConfirm(___("Confirm end creation", "ddui_tuto:wizard"));
+        $menu->appendElement($item);
+
+        $item = new ItemMenu(
+            "wizard_next", ___("Next step", "ddui_tuto:wizard"),
+            "#action/wizard.next"
+        );
+        $item->setBeforeContent('<div class="fa fa-step-forward" />');
+        $item->setHtmlAttribute("class", "menu--right");
+        if (empty($this->wizardInfos['nextStep'])) {
+            $item->setVisibility($item::VisibilityHidden);
+        }
+        $menu->appendElement($item);
+
+        $item = new ItemMenu(
+            "wizard_previous", ___("Previous step", "ddui_tuto:wizard"),
+            "#action/wizard.previous"
+        );
+        $item->setBeforeContent('<div class="fa fa-step-backward" />');
+        $item->setHtmlAttribute("class", "menu--right");
+        if (empty($this->wizardInfos['previousStep'])) {
+            $item->setVisibility($item::VisibilityDisabled);
+        }
+        $menu->appendElement($item);
+
+        $item = new ItemMenu(
+            "wizard_cancel", ___("Cancel", "ddui_tuto:wizard"),
+            "#action/wizard.cancel"
+        );
+        $item->setBeforeContent('<div class="fa fa-undo" />');
+        $item->setHtmlAttribute("class", "menu--left");
+        $menu->appendElement($item);
+
+        return $menu;
     }
 }
