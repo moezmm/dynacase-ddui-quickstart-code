8feeb63b8def317fac8dd16817f366d74a35498c
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-19 15:49:23 +0100

    Mise en place des vues - Partie serveur : mise en place du header
    --
    tag: step-40-30

--- DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactWizardRenderConfigEdit.php
+++ DDUI_TUTO/Families/DDUI_TUTO_CONTACT/ContactWizardRenderConfigEdit.php
@@ -5,4 +5,40 @@ namespace DduiTuto;
 class ContactWizardRenderConfigEdit extends \Dcp\Ui\DefaultEdit
 {
     use TContactWizardRenderConfigEdit;
+
+    public function getContextController(\Doc $document)
+    {
+        $this->initWizardInfos($document);
+
+        $controller = parent::getContextController($document);
+        $controller["wizardInfos"] = $this->wizardInfos;
+        return $controller;
+    }
+
+    public function getTemplates(\Doc $document = null)
+    {
+        $this->initWizardInfos($document);
+
+        $templates = parent::getTemplates($document);
+
+        $templates["sections"]["menu"]["file"]
+            = "DDUI_TUTO/Families/DDUI_TUTO_CONTACT/Layout/contactWizardHeader.mustache";
+
+        return $templates;
+    }
+
+    public function getCssReferences(\Doc $document = null)
+    {
+        $version = \ApplicationParameterManager::getParameterValue(
+            "CORE", "WVERSION"
+        );
+
+        $cssReferences = parent::getCssReferences($document);
+
+        $cssReferences['DDUI_TUTO_CONTACT_WIZARD']
+            = "DDUI_TUTO/Families/DDUI_TUTO_CONTACT/Layout/wizard.css?ws="
+            . $version;
+
+        return $cssReferences;
+    }
 }
