933bc7797ef1955bbe3a1c1a45561d2e825a76ef
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-19 17:08:57 +0100

    Mise en place des vues - Partie client : Gestion des menus
    --
    tag: step-40-80

--- DDUI_TUTO/Families/DDUI_TUTO_CONTACT/Layout/wizard.js
+++ DDUI_TUTO/Families/DDUI_TUTO_CONTACT/Layout/wizard.js
@@ -37,6 +37,75 @@
         },
         function addWizardEvents(/*event, documentObject*/)
         {
+            // react to actionClick events from the menus
+            this.documentController(
+                "addEventListener",
+                "actionClick",
+                {
+                    "name": "actionClick.wizard.contact",
+                    "documentCheck": function actionClickWizardContactDocumentCheck(documentObject)
+                    {
+                        return documentObject.family.name === "DDUI_TUTO_CONTACT" &&
+                            documentObject.renderMode === "edit";
+                    }
+                },
+                function actionClickWizardContact(event, documentObject, options)
+                {
+                    var customServerData = this.documentController("getCustomServerData"),
+                        currentWizardStepName = null;
+
+                    if(customServerData && customServerData.wizardInfos && customServerData.wizardInfos.currentStep) {
+                        currentWizardStepName = customServerData.wizardInfos.currentStep.cv_idview;
+                    }
+
+                    switch (options.eventId) {
+                        case 'wizard.cancel':
+                            // reinit document to server state
+                            this.documentController(
+                                "reinitDocument",
+                                {
+                                    viewId: '!defaultConsultation'
+                                }
+                            );
+                            break;
+                        case 'wizard.previous':
+                        case 'wizard.next':
+                            // save document and send customClientData with goto and currentStepName
+                            this.documentController(
+                                "saveDocument",
+                                {
+                                    customClientData: {
+                                        goto: options.eventId,
+                                        currentWizardStepName: currentWizardStepName
+                                    }
+                                }
+                            ).then(function actionClickWizardContact_success(successInfos)
+                            {
+                                successInfos.element.documentController(
+                                    "showMessage",
+                                    "Document has been saved"
+                                );
+                            }, function actionClickWizardContact_error(errorInfos)
+                            {
+                                errorInfos.element.documentController(
+                                    "showMessage",
+                                    {
+                                        type: 'error',
+                                        message: 'an error occured: ' + errorInfos.errorMessage.contentText
+                                    }
+                                );
+                            });
+                            break;
+                        case 'wizard.end':
+                            //TODO
+                            break;
+                        default:
+                            return;
+                    }
+                    event.preventDefault();
+                }
+            );
+            
             // update the header summary according to attributes values
             updateSummaryStatus();
         }
