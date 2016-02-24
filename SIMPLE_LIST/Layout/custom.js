bc8fa0307344b42ffef9fa0792b64694b1c8cb26
Matthieu Codron <matthieu.codron+dev@anakeen.com>
2016-02-24 17:54:02 +0100

    Chargement d'un document
    --
    tag: step-50-10

--- SIMPLE_LIST/Layout/custom.js
+++ SIMPLE_LIST/Layout/custom.js
@@ -3,5 +3,42 @@
     "use strict";
 
     var $window = $(window),
-        $documentWrapper = $('.documentWrapper');
+        $documentWrapper = $('.documentWrapper'),
+        loadDocument = function loadDocument(fetchOptions)
+        {
+            //check if the widget has already been initialised
+            if (_.isUndefined($documentWrapper.document("instance"))) {
+                //if not, initialize it
+                $documentWrapper.document(fetchOptions)
+                    //and attach listeners to the newly created widget
+                    .on("documentloaded", addDocumentListeners);
+            } else {
+                //if yes, reuse it
+                $documentWrapper.document("fetchDocument", fetchOptions);
+            }
+        },
+        addDocumentListeners = function addDocumentListeners()
+        {
+            // propagate that this document is opened each time it is reloaded
+            $documentWrapper.document(
+                "addEventListener",
+                "ready",
+                {
+                    "name": "ready.simple_list"
+                },
+                function simpleList_propagateReady(/*event, documentObject*/)
+                {
+                    // refresh the currently selected document
+                    $window.trigger('documentOpened', $documentWrapper.document("getProperties"));
+                }
+            );
+        };
+
+    $window.on('documentElementClicked', function onDocumentElementClicked(event, options)
+    {
+        loadDocument({
+            "initid": options.initid,
+            "viewId": "!defaultConsultation"
+        });
+    });
 })($, _);
